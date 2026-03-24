9375ee50d70afaffaf30553e0076b2f53a556ccc
tutoOmar <omar-ortiz@javeriana.edu.co>
2026-03-24 17:31:48 -0500

    feature: Add distanciaA method to Coordenada and corresponding unit tests

--- backend/src/test/java/com/foodtech/domain/model/CoordenadaTest.java
+++ backend/src/test/java/com/foodtech/domain/model/CoordenadaTest.java
@@ -31,4 +31,34 @@ class CoordenadaTest {
     void shouldThrowException_WhenValuesExceedLimit() {
         assertThrows(IllegalArgumentException.class, () -> new Coordenada(101, 50));
     }
+
+    @Test
+    void distancia_shouldReturnZero_whenSameCoordinates() {
+        Coordenada a = new Coordenada(10, 20);
+        assertEquals(0.0, a.distanciaA(a), 1e-6);
+    }
+
+    @Test
+    void distancia_shouldReturnFive_for3_4_5Triangle() {
+        Coordenada a = new Coordenada(0, 0);
+        Coordenada b = new Coordenada(3, 4);
+        assertEquals(5.0, a.distanciaA(b), 1e-6);
+    }
+
+    @Test
+    void distancia_shouldBeSymmetric_betweenPoints() {
+        Coordenada a = new Coordenada(10, 10);
+        Coordenada b = new Coordenada(13, 14);
+        double d1 = a.distanciaA(b);
+        double d2 = b.distanciaA(a);
+        assertEquals(d1, d2, 1e-9);
+        assertEquals(5.0, d1, 1e-6);
+    }
+
+    @Test
+    void distancia_shouldReturnSqrt2_forDiagonalUnit() {
+        Coordenada a = new Coordenada(0, 0);
+        Coordenada b = new Coordenada(1, 1);
+        assertEquals(Math.sqrt(2), a.distanciaA(b), 1e-6);
+    }
 }
